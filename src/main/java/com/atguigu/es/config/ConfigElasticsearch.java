671657449679bc6eebb94e1656248e5e671f4e20
zhou <hl281213@163.com>
2023-05-17 20:25:16 +0800

    dev 周海龙

---
+++ src/main/java/com/atguigu/es/config/ConfigElasticsearch.java
@@ -0,0 +1,24 @@
+package com.atguigu.es.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration //配置类
+public class ConfigElasticsearch {
+
+    //利用RestHighLevelClient操作
+    public RestHighLevelClient esRestClient(){
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("192.168.179.128",9200,"http"))
+        );
+        return client;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

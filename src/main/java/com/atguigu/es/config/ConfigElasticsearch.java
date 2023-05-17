493661c1c56c80128228b59bb82b42a00a3a4ade
zhou <hl281213@163.com>
2023-05-17 20:39:42 +0800

    dev 周海龙第1次更改

--- src/main/java/com/atguigu/es/config/ConfigElasticsearch.java
+++ src/main/java/com/atguigu/es/config/ConfigElasticsearch.java
@@ -21,5 +21,6 @@ public class ConfigElasticsearch {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
